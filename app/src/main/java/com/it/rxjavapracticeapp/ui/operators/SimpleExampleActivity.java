90e0a0985d9e73f66c18e3e39be241bba889e22d
Mukesh <pranavved.1@gmail.com>
2019-03-17 14:00:36 +0530

    RaJava Setup

--- app/src/main/java/com/it/rxjavapracticeapp/ui/operators/SimpleExampleActivity.java
+++ app/src/main/java/com/it/rxjavapracticeapp/ui/operators/SimpleExampleActivity.java
@@ -10,6 +10,7 @@ public class SimpleExampleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_example);
     }
