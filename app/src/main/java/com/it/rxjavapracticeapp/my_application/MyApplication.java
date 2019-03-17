bab43713ab9000743bb5d429c88f4ebae689eb05
Mukesh <pranavved.1@gmail.com>
2019-03-17 14:11:07 +0530

    RaJava Setup

--- app/src/main/java/com/it/rxjavapracticeapp/my_application/MyApplication.java
+++ app/src/main/java/com/it/rxjavapracticeapp/my_application/MyApplication.java
@@ -6,6 +6,21 @@ import android.app.Application;
 /**
  * Created by threshold on 2017/1/12.
  */
+/**********************************************************************************************************************************/
+/**********************************************************************************************************************************/
+    /*
+        You will have to learn by doing for RxJava. This repository
+        (https://github.com/amitshekhariitbhu/RxJava2-Android-Samples) has all the
+        RxJava samples implemented in various activities. You will have to write codes
+        parallel to the examples provided in the repository in your own separate personal
+        project. More you do the better for your but we expect these to be done Map, Zip,
+        Filter, FlatMap, Merge, SwitchMap. They need to be implemented as explained in
+        the Repository.
+    */
+/**********************************************************************************************************************************/
+/**********************************************************************************************************************************/
+
+
 
 public class MyApplication extends Application {
 
